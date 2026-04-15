b786be7c1b2b5f5333c497a565553ff304c6cdfe
Aditya Singh Chauhan <ac3413452@gmail.com>
2026-04-15 23:27:14 +0530

    chore: update frontend configuration and global styling

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -7,11 +7,23 @@ const nextConfig: NextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:7007/api/:path*", // Proxy to Backend
+        destination: "http://127.0.0.1:8000/api/:path*", // Proxy to Backend
       },
     ];
   },
